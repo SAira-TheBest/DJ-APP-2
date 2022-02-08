1ba66c21537ad1938bc53d835e13ddfddaef4f8e
SAira-TheBest <82064943+SAira-TheBest@users.noreply.github.com>
2022-02-08 15:55:55 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,53 @@
-function setup(){
-    canvas= createCanvas(560,560)
-    canvas.position(0,0)
+song =""
+leftWristX = 0
+leftWristY = 0
+rightWristX = 0
+rightWristY = 0
+scoreLeftWrist = 0
 
-    video = createCapture(VIDEO)
-    video.hide()
+function setup(){
+
+ canvas = createCanvas(600, 500)
+ canvas.center()
+
+video = createCapture(VIDEO)
+video.hide()
+
+poseNet= ml5.poseNet(video, modelLoaded)
+poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded(){
+    console.log('Saira')
 }
 
 function preload(){
-    song = loadSound("music.mp3")
-    song = loadSound("music2.mp3")
+song = loadSound("music.mp3")
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500)
+}
+
+function play(){
+song.play()
+song.rate(1)
+}
+
+function gotPoses(results)
+{
+
+    if(results.length > 0)
+    {  scoreLeftWrist = results[0].pose.keypoints[9].score
+        console.log("scoreLeftWrist =" + scoreLeftWrist);
+
+        console.log(results)
+        leftWristX = results[0].pose.leftWrist.x
+        leftWristY = results[0].pose.rightWrist.y
+        console.log("leftWristX =" + leftWristX +" LeftWristY =" + leftWristY)
+
+        rightWristX = results[0].pose.rightWrist.x
+        rightWristY = results[0].pose.rightWrist.y
+        console.log("rightWristX =" + rightWristX +" rightWristY =" + rightWristY)
     }
-    
-    function draw(){
-        image(video, 0, 0, 600, 500)
-    }
+}
